22b348b2500ff56a840833c1383f5211940c176e
Irobot <skuwar@ufl.edu>
2018-03-05 22:07:08 -0500

    map updated

--- _site/talkmap/org-locations.js
+++ _site/talkmap/org-locations.js
@@ -1,22 +1,47 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Adaptive evolution of serine protease superfamily and transcriptional response to a plant\nprotease inhibitor in *Helicoverpa armigera*. | Dornburg castle, Jena, GERMANY",
+    50.9281717,
+    11.5879359
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Differential regulation of serine protease multigene family against a plant protease inhibitor in *Helicoverpa armigera*. | Scientific Advisory Board Meeting 2012, Jena, GERMANY",
+    50.9281717,
+    11.5879359
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Annotation of serine protease superfamily and transcriptional response to a plant protease inhibitor in *Helicoverpa armigera*. |  6th Annual Arthropod Genomic Symposium, Kansas City, UNITED STATES",
+    39.0844687,
+    -94.5630298
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Molecular evolution of serine protease superfamily and adaptive response to a plant protease inhibitor in *Helicoverpa armigera*.  | Scientific Advisory Board Meeting 2014, Jena, GERMANY",
+    50.9281717,
+    11.5879359
+  ],
+  [
+    " Molecular evolution of serine protease superfamily and adaptive response to soybean Kunitz trypsin inhibitor in *Helicoverpa armigera*. | International Chemical Ecology Conference 2013, International Society of Chemical Ecology, Asia Pacific\nAssociation of Chemical Ecologists, Melbourne, AUSTRALIA",
+    -37.8142176,
+    144.9631608
+  ],
+  [
+    " Molecular evolution of digestive serine proteases of *Helicoverpa armigera*. |   International Max Planck Research School Symposium, Jena, GERMANY",
+    50.9281717,
+    11.5879359
+  ],
+  [
+    "Transcriptional and translational response to a plant protease inhibitor and Annotation of\nserine protease superfamily in *Helicoverpa armigera*. | South China Normal University, Guangzhou, CHINA",
+    23.1301964,
+    113.2592945
+  ],
+  [
+    " Adaptive evolution and regulation of digestive serine protease superfamily in *Helicoverpa armigera*. |   Institute of Chemical Ecology Symposium, Jena, GERMANY",
+    50.9281717,
+    11.5879359
+  ],
+  [
+    " Kinetic modeling of thermal inactivation of soluble and immobilized aminoacylase on novel acrylic beaded polymers. |    Industry-Institute of Bio-informatics and biotechnology meeting., Pune, INDIA",
+    18.5203062,
+    73.8543185
   ]
 ];
